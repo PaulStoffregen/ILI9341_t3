7d239285cb6025ae706c7a80466b6699e4b81095
PaulStoffregen <paul@pjrc.com>
2015-08-17 09:53:52 -0700

    Add full Arial font

---
+++ font_ArialBoldItalic.h
@@ -0,0 +1,33 @@
+#ifndef _ILI9341_t3_font_ArialBoldItalic_
+#define _ILI9341_t3_font_ArialBoldItalic_
+
+#include "ILI9341_t3.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const ILI9341_t3_font_t Arial_8_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_9_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_10_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_11_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_12_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_13_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_14_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_16_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_18_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_20_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_24_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_28_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_32_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_40_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_48_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_60_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_72_Bold_Italic;
+extern const ILI9341_t3_font_t Arial_96_Bold_Italic;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif
---
+++ font_ArialItalic.h
@@ -0,0 +1,33 @@
+#ifndef _ILI9341_t3_font_ArialItalic_
+#define _ILI9341_t3_font_ArialItalic_
+
+#include "ILI9341_t3.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const ILI9341_t3_font_t Arial_8_Italic;
+extern const ILI9341_t3_font_t Arial_9_Italic;
+extern const ILI9341_t3_font_t Arial_10_Italic;
+extern const ILI9341_t3_font_t Arial_11_Italic;
+extern const ILI9341_t3_font_t Arial_12_Italic;
+extern const ILI9341_t3_font_t Arial_13_Italic;
+extern const ILI9341_t3_font_t Arial_14_Italic;
+extern const ILI9341_t3_font_t Arial_16_Italic;
+extern const ILI9341_t3_font_t Arial_18_Italic;
+extern const ILI9341_t3_font_t Arial_20_Italic;
+extern const ILI9341_t3_font_t Arial_24_Italic;
+extern const ILI9341_t3_font_t Arial_28_Italic;
+extern const ILI9341_t3_font_t Arial_32_Italic;
+extern const ILI9341_t3_font_t Arial_40_Italic;
+extern const ILI9341_t3_font_t Arial_48_Italic;
+extern const ILI9341_t3_font_t Arial_60_Italic;
+extern const ILI9341_t3_font_t Arial_72_Italic;
+extern const ILI9341_t3_font_t Arial_96_Italic;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif

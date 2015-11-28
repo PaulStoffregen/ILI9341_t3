f23cc88c0c444614cb96cba22fabede02562eff3
Dean Blackketter <dean@blackketter.com>
2015-11-28 15:29:58 -0800

    Another minor clipping bug that was causing some lines on the left side of the clipping area to be incorrect.

--- ILI9341_t3.h
+++ ILI9341_t3.h
@@ -134,7 +134,7 @@ class ILI9341_t3 : public Print
   public:
 	ILI9341_t3(uint8_t _CS, uint8_t _DC, uint8_t _RST = 255, uint8_t _MOSI=11, uint8_t _SCLK=13, uint8_t _MISO=12);
 	void begin(void);
-  	void sleep(bool enable);		
+	void sleep(bool enable);
 	void pushColor(uint16_t color);
 	void fillScreen(uint16_t color);
 	void drawPixel(int16_t x, int16_t y, uint16_t color);
@@ -293,7 +293,7 @@ class ILI9341_t3 : public Print
 
     // Rudimentary clipping
     if((y < _clipy1) || (x >= _clipx2) || (y >= _clipy2)) return;
-    if(x<_clipx1) { w = w - (_clipx1 - w); x = _clipx1; }
+    if(x<_clipx1) { w = w - (_clipx1 - x); x = _clipx1; }
     if((x+w-1) >= _clipx2)  w = _clipx2-x;
     if (w<1) return;
 

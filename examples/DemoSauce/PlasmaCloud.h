f9f10ea8c1ce3e93f672c9b1e17fa71bc6a7cc25
PaulStoffregen <paul@pjrc.com>
2023-11-22 02:27:51 -0800

    Fix compile error in DemoSauce PlasmaCloud

--- examples/DemoSauce/PlasmaCloud.h
+++ examples/DemoSauce/PlasmaCloud.h
@@ -77,12 +77,12 @@ void PlasmaCloud::perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
 	//py._ditherY = (py._ditherY + 1) % PLASMA_YELLOW_DITHER;
 
 	PointU16 p0 = (PointU16){
-		(uint_fast16_t)(w/2 + (sin(_phase*0.32f)*(w/2-PLASMA_CLOUD_MARGIN) )),
-		(uint_fast16_t)(h/2 + (sin(_phase*0.23f)*(h/2-PLASMA_CLOUD_MARGIN) ))
+		(uint_fast16_t)(w/2 + (sinf(_phase*0.32f)*(w/2-PLASMA_CLOUD_MARGIN) )),
+		(uint_fast16_t)(h/2 + (sinf(_phase*0.23f)*(h/2-PLASMA_CLOUD_MARGIN) ))
 	};
 	PointU16 p1 = (PointU16){
-		(uint_fast16_t)(w/2 + (cos(_phase*1.07f)*(w/2-PLASMA_CLOUD_MARGIN) )),
-		(uint_fast16_t)(h/2 + (cos(_phase*1.42f)*(h/2-PLASMA_CLOUD_MARGIN) ))
+		(uint_fast16_t)(w/2 + (cosf(_phase*1.07f)*(w/2-PLASMA_CLOUD_MARGIN) )),
+		(uint_fast16_t)(h/2 + (cosf(_phase*1.42f)*(h/2-PLASMA_CLOUD_MARGIN) ))
 	};
 	/*
 	PointU16 p2 = (PointU16){
@@ -93,12 +93,18 @@ void PlasmaCloud::perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
 
 	for( uint_fast16_t x=0; x<w; x+=PLASMA_CLOUD_LINE_WIDTH ) {
 		for( uint_fast16_t y=_ditherY; y<h; y+=PLASMA_CLOUD_STEP_Y ) {
-			PointU8 d0 = (PointU8){ abs(p0.x - x), abs(p0.y - y) };
-			PointU8 d1 = (PointU8){ abs(p1.x - x), abs(p1.y - y) };
+			//PointU8 d0 = (PointU8){ abs(p0.x - x), abs(p0.y - y) };
+			//PointU8 d1 = (PointU8){ abs(p1.x - x), abs(p1.y - y) };
 			//PointU8 d2 = (PointU8){ abs(p2.x - x), abs(p2.y - y) };
+			// newer compiler gives abs ambiguous overload error
+			// simply do this math as int to work around the issue
+			int d0_x = (int)p0.x - (int)x;
+			int d0_y = (int)p0.y - (int)y;
+			int d1_x = (int)p1.x - (int)x;
+			int d1_y = (int)p1.y - (int)y;
 
-			uint_fast8_t lookup0 = (d0.x*d0.x + d0.y*d0.y) >> sqrtBitShift;
-			uint_fast8_t lookup1 = (d1.x*d1.x + d1.y*d1.y) >> sqrtBitShift;
+			uint_fast8_t lookup0 = (d0_x*d0_x + d0_y*d0_y) >> sqrtBitShift;
+			uint_fast8_t lookup1 = (d1_x*d1_x + d1_y*d1_y) >> sqrtBitShift;
 			//uint_fast8_t lookup2 = (d2.x*d2.x + d2.y*d2.y) >> sqrtBitShift;
 
 			uint_fast8_t bright = (sqrtTable[ lookup0 ] * sqrtTable[ lookup1 ] ) >> 6;
